3f3b518e48517e5727bb989d29bda5c017da7581
Matthieu PERREIRA DA SILVA <matthieu@perreira.net>
2021-01-08 14:36:32 +0100

    Added all projects
    This should be fine for starting developping ;-)

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Hyblab2021
-Source code of the Hyblab 2021 projects
+![Hyblab 2021](__common-logos__/hyblab_2021.png)
+
+Source code of all the projects of [Hyblab 2021][2]. Hyblab 2021 is an interactive data-service project organized by [Ouest Medilab][3] in the city of Nantes (France) from January 14 to February 2, 2021.
+
+Three schools participated in this project:
+
+ - [SciencesCom][4] (journalism)
+ - [Polytech Nantes][5] (computer science)
+ - [AGR-L'école de l'image][6] (web design)
+
+
+This year, all 10 projects where proposed by Ouest Médialab for his partner medias.
+
+[2]: http://www.hyblab.fr
+[3]: http://www.ouestmedialab.fr
+[4]: http://www.sciencescom.org
+[5]: http://www.polytech.univ-nantes.fr
+[6]: http://www.agrnantes.fr
+
+# How to run the code ?
+
+- Download the code...
+- Install a recent version of nodejs
+- Type `npm install` at the root of the code folder
+- Type `npm start` to launch a local server at 127.0.0.1 on port 8080
+- You can now visit any project at http://127.0.0.1:8080/name_of_the_projet. For example: http://127.0.0.1:8080/simple-example
---
+++ server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// general routing framework
+var express = require('express')
+//var basicAuth = require('basic-auth-connect');
+var app = express()
+
+// password protection
+//app.use(basicAuth('story2020', 'ilovestory'));
+
+// declare the list of sub apps
+var app_names = [];
+
+var hyblab2021_names = ['boulot-a', 'boulot-b', 'mer-a', 'mer-b', 'parents-a',
+'parents-b', 'proximite-a', 'proximite-b', 'velo-a', 'velo-b'];
+
+app_names.push.apply(app_names, hyblab2021_names);
+
+var sub_apps = [];
+
+// create sub apps
+// and register sub-apps
+app_names.forEach( function( element, index, array) {
+  console.log('Registering: ' + element);
+	sub_apps[element] = require('./' + element + '/server');
+	app.use('/' + element, sub_apps[element]);
+});
+
+// redirect catch all url to hyblab website (disabled for dev)
+//app.use(/\/$/,function(req, res, next){
+//	res.redirect('http://www.hyblab.fr/');
+//});
+
+
+// launch main server app
+
+//warning:  do not change the port, it will be automatically taken from env en dev and prod servers ...
+var port = 'PORT' in process.env ? process.env.PORT : 8080;
+var server = app.listen(port, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Hyblab routing app listening at http://%s:%s', host, port)
+
+})
