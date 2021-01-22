ed86abb01e4b31b9caac708b3ba1d682796e7f54
Your Name <you@example.com>
2021-01-22 21:58:23 +0100

    BUG FIX : Modification de la route principale pour gérer l'async

--- mer-a/server.js
+++ mer-a/server.js
@@ -5,25 +5,57 @@
 var express = require('express');
 var path = require('path');
 
+var Legende = require('./class/Legende.js')
+
 var app = express();
 
 const sqlite3 = require('sqlite3').verbose();
+const { open } = require('sqlite');
 
 // open database in memory
-let db = new sqlite3.Database('./db/database.db', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory SQlite database.');
+let db = null;
+(async () => {
+  db = await open({filename: './db/database.db', driver: sqlite3.Database});
+})()
+
+app.use(`/api/:region/:typeHistoire`, async (req, res) => {
+    var legendes = [];
+    var sql = `SELECT * FROM Legende 
+                WHERE departement = "${encodeURI(req.params.region)}"
+                AND categorie = "${encodeURI(req.params.typeHistoire)}"`;
+    console.log(sql);
+
+    const rows = await db.all(sql, []);  
+    rows.forEach((row) => {
+        console.log(decodeURI(row.nom));
+        var legende = new Legende(
+            row.nom, 
+            row.departement, 
+            row.categorie, 
+            row.resume, 
+            row.histoire, 
+            row.latitude, 
+            row.longitude, 
+            row.adresse,
+            row.baignade, 
+            row.toilettes, 
+            row.restaurant, 
+            row.photo);
+        legendes.push(legende);
+    });
+    console.log(legendes);
+    res.status(200)
+    res.json({data:legendes});
 });
 
+
 // close the database connection
-db.close((err) => {
+/*db.close((err) => {
   if (err) {
     return console.error(err.message);
   }
   console.log('Close the database connection.');
-});
+});*/
 
 // Minimum routing: serve static content from the html directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,4 +67,5 @@ app.use(express.static(path.join(__dirname, '../__common-logos__')));
 // at the root of this set of projects. You can access it by lanching the main
 // server and visiting http(s)://127.0.0.1:8080/name_of_you_project/ (if on a local server)
 // or more generally: http(s)://server_name:port/name_of_you_project/
-module.exports = app;
+app.listen(8080);
+//module.exports = app;
