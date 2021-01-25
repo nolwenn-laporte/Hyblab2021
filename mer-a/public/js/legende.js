c78103b6139599e0cb10549be0a0df07aaaa4e81
titouan.guyader@etu.univ-nantes.fr <titouan.guyader@free.fr>
2021-01-25 12:26:01 +0100

    page finale ajout texte v1

--- mer-a/public/js/legende.js
+++ mer-a/public/js/legende.js
@@ -6,7 +6,9 @@
     departement: 'Finistère', 
     categorie: 'Histoires Maritimes', 
     resume: 'Une plage macabre qui effraie les marrins de toute la Bretagne ...', 
-    histoire: `En breton on l’appelle Boë an Anao, qui signifie : la « baie des âmes en peine ». On raconte que les corps des marins qui avaient perdu la vie en mer venaient s’y échouer. Durant la nuit de Noël la baie résonne des chants des âmes en peine ballotées sur le bateau des morts. Une autre légende rapporte qu’elle servait de lieu d’embarquement des dépouilles des druides morts vers l’Île de Sein pour leur inhumation.`,
+    histoire: `En breton on l’appelle Boë an Anao, qui signifie : la « baie des âmes en peine ». On raconte que les corps des marins qui avaient perdu la vie en mer venaient s’y échouer. Durant la nuit de Noël la baie résonne des chants des âmes en peine ballotées sur le bateau des morts. Une autre légende rapporte qu’elle servait de lieu d’embarquement des dépouilles des druides morts vers l’Île de Sein pour leur inhumation.
+    En breton on l’appelle Boë an Anao, qui signifie : la « baie des âmes en peine ». On raconte que les corps des marins qui avaient perdu la vie en mer venaient s’y échouer. Durant la nuit de Noël la baie résonne des chants des âmes en peine ballotées sur le bateau des morts. Une autre légende rapporte qu’elle servait de lieu d’embarquement des dépouilles des druides morts vers l’Île de Sein pour leur inhumation.`,
+
     latitude: '48.04730', 
     longitude: '-4.70676',
     adresse: 'Cléden-Cap-Sizun 29770',
@@ -18,6 +20,7 @@
 
 	document.querySelector('#nom').innerHTML = data.nom;
 	document.querySelector('#department').innerHTML = data.departement;
+	document.querySelector('div#bubble').innerHTML = data.histoire;
 	document.querySelector('#categorie').innerHTML = data.categorie;
 	document.querySelector('#adresse').innerHTML = data.adresse;
 	document.querySelector('#photo').src = data.photo;
